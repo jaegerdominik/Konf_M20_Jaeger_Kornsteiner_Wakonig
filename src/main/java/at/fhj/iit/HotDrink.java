3c9135ae9cde9696f05b06fc05e40e092854ec41
Gregor Wakonig <gregor.wakonig@edu.fh-joanneum.at>
2021-03-15 17:32:03 +0100

    Added implementation of the first two methods in HotDrink.java

--- src/main/java/at/fhj/iit/HotDrink.java
+++ src/main/java/at/fhj/iit/HotDrink.java
@@ -1,6 +1,34 @@
+/*
+ * Class: 	 HotDrink
+ * Description:  Class wich extends Drink and represents a Hot Drink recipe
+ * Author: 		Gregor Wakonig
+ * Last Change: 	15.03.2021
+ */
+
 package at.fhj.iit;
 
-public class HotDrink {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    //first impmlementation
+public class HotDrink extends Cocktail{
+
+    /**
+     * Creates a Drink object with given name, e.g. juice or orange juice
+     *
+     * @param name        name of the drink
+     * @param ingredients
+     */
+    public HotDrink(String name, Liquid... ingredients) {
+        super(name, ingredients);
+    }
+
+
+    public void calculateTemperature() {
+    }
+
+
+    public void printHotDrink() {
+    }
 }
+

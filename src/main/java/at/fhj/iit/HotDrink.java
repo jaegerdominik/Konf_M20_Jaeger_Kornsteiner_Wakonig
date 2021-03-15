3fa320bb26502f32ae8df8f9e1252ed914305e5a
Gregor Wakonig <gregor.wakonig@edu.fh-joanneum.at>
2021-03-15 18:09:54 +0100

    Added comments to HotDrink.java and refined the print method in HotDrink.Java

--- src/main/java/at/fhj/iit/HotDrink.java
+++ src/main/java/at/fhj/iit/HotDrink.java
@@ -11,7 +11,7 @@ package at.fhj.iit;
 public class HotDrink extends Cocktail{
     private double temperature = 0.0D;
     /**
-     * Creates a Drink object with given name, e.g. juice or orange juice
+     * Creates a drink object with given name, e.g. juice or orange juice
      *
      * @param name        name of the drink
      * @param ingredients
@@ -20,36 +20,35 @@ public class HotDrink extends Cocktail{
         super(name, ingredients);
     }
 
-
+    // Method to calculate the temperature of the liquids that are mixed together
     public void calculateTemperature() {
         float divisor = 0;
         float dividend = 0;
 
         for (Liquid liquid : ingredients)
         {
-            //System.out.println(liquid.getVolume());
             divisor += (liquid.getTemperature() * liquid.getVolume());
             dividend += (liquid.getVolume());
-
         }
-        //System.out.println("divisor: " + divisor);
-        //System.out.println("dividend: " + dividend);
         if (dividend > 0 && divisor > 0){
             this.temperature = divisor/dividend;
             System.out.printf("%s temperature: %.2f degree Celsius", name, temperature);
         }
         else {
-            System.out.println("Specify all parameters to see the Temperature!");
+            System.out.println("Specify all parameters of the liquids to see the temperature!");
         }
-
-
     }
-
 
     public void printHotDrink() {
         System.out.printf("\n----%s----", name);
-        System.out.println("\nit's soo good");
-        System.out.printf("Temperature: %.1f", temperature);
+        System.out.println("\nIt's soo good!");
+        System.out.println("Contains the following liquids:");
+        for (Liquid liquid : ingredients)
+        {
+            System.out.printf("%.3f liters of %s at %.0f degree Celsius\n", liquid.getVolume(),liquid.getName(), liquid.getTemperature());
+        }
+
+        System.out.printf("Temperature: %.1f degree Celsius", temperature);
     }
 }
 
